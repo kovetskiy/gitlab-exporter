e95fed4c3f709df52deaaebf38944b1142796450
Mic Szillat <mic@nomaster.cc>
2017-03-17 09:48:05 +0100

    :sparkles: observe project build times

--- gitlab_exporter.py
+++ gitlab_exporter.py
@@ -3,10 +3,11 @@
 
 import logging
 import os
+from datetime import datetime
 import time
 
 import gitlab
-from prometheus_client import start_http_server, Gauge
+from prometheus_client import start_http_server, Summary
 
 try:
     loglevel = getattr(logging, os.environ.get('LOGLEVEL', 'WARN').upper())
@@ -23,27 +24,50 @@ INTERVAL = int(os.environ.get('INTERVAL', 300))
 # port to listen on
 PORT = int(os.environ.get('PORT', 3001))
 
-# This expects a valid config with default server. See
-# http://python-gitlab.readthedocs.io/en/stable/cli.html#configuration
-gl = gitlab.Gitlab.from_config()
+# URL of the GitLab instance, defaults to hosted GitLab
+URL = str(os.environ.get('URL', 'https://gitlab.com'))
+
+# Secret token for the app to authenticate itself
+TOKEN = str(os.environ.get('TOKEN'))
+
+# Login to GitLab
+gl = gitlab.Gitlab(URL, TOKEN)
+gl.auth()
+
+# Initialize Prometheus instrumentation
+gitlab_project_build_time = Summary('gitlab_project_build_time', 'Time the project builds took to run', ['project_id'])
 
 
-sidekiq_jobs_enqueued = Gauge('sidekiq_jobs_enqueued', 'Total number of enqueued Sidekiq jobs')
-sidekiq_jobs_failed = Gauge('sidekiq_jobs_failed', 'Total number of failed Sidekiq jobs')
-sidekiq_jobs_processed = Gauge('sidekiq_jobs_processed', 'Total number of processed Sidekiq jobs')
-gitlab_project_count = Gauge('gitlab_project_count', 'Number of projects')
-gitlab_user_count = Gauge('gitlab_user_count', 'Number of users')
+def get_projects():
+    projects = gl.projects.list()
+    return projects
+
+
+def get_project_builds(project):
+    id = project.id
+    project_builds = gl.project_builds.list(project_id=id)
+    return project_builds
+
+
+def get_build_time(build):
+    try:
+        build_start = datetime.strptime(build.started_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+        build_end = datetime.strptime(build.finished_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+        build_time = build_end - build_start
+        return build_time
+    except TypeError:
+        pass
 
 
 def get_stats():
-    log.info('fetching project count')
-    gitlab_project_count.set(int(gl._raw_get('/projects/all').headers['X-Total']))
-    gitlab_user_count.set(int(gl._raw_get('/users').headers['X-Total']))
-    x = gl.sidekiq.job_stats()
-    # {'jobs': {'enqueued': 0, 'failed': 140300, 'processed': 211394}}
-    sidekiq_jobs_enqueued.set(x['jobs']['enqueued'])
-    sidekiq_jobs_failed.set(x['jobs']['failed'])
-    sidekiq_jobs_processed.set(x['jobs']['processed'])
+    for project in get_projects():
+        project = gl.projects.get(project.id)
+        for build in get_project_builds(project):
+            try:
+                build_time = get_build_time(build)
+                gitlab_project_build_time.labels(project_id=project.id).observe(build_time.total_seconds())
+            except AttributeError:
+                pass
 
 
 if __name__ == '__main__':
